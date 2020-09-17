d6591288fdd4e1042d1fa147a2a3ff73d177a846
mccray austin <tubach75@hotmail.com>
2020-09-17 14:55:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
-grandma = "what is going in here?"
+grandma = "I LOVE YOU GRANDMA"
 def speak_to_grandma(grandma)
   if grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
