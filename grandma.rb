3741148ca76ef7fcf36cb70d541402823338731c
mccray austin <tubach75@hotmail.com>
2020-09-17 15:09:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 string = "I LOVE YOU GRANDMA"
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!" && string == string.upcase
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif string == string.upcase
     puts "NO, NOT SINCE 1938"
   else
